efd4559f3417f5e2239ccfc891b00a02a23dc7a1
dsinha2020 <71216400+dsinha2020@users.noreply.github.com>
2023-08-05 19:11:05 +0530

    Add files via upload

--- SP3_FOT_DDM/interface_mm.m
+++ SP3_FOT_DDM/interface_mm.m
@@ -1,23 +1,31 @@
-% function RHS_extra=interface_mm(partmat,NodeCoord,N)
 function ABC=interface_mm(partmat,NodeCoord,N,beta)
+
+% This function gives the assembled matrix corresponding to interface nodes for each subdomain
+
+%%%%%%%%% INPUTS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% partmat: structure type variable containing subdomain info
+% NodeCoord: nodal coordinate matrix for the entire undivided domain 
+% N: Number of subdomains
+% beta: penalty coeffcient (extra parameter along the interface) defined by Deng in his approach (method 2)
+
+%%%%%%%%%%% OUTPUT %%%%%%%%%%%%%%%%%%%%%%%%%
+% ABC: Structure type variable with fields corresponding to interface matrix for each interface of a given subdomain
+%       (Example: Subdomain 2 would have interfaces along j=1 & 3. Therefore, 1st and 3rd row would be filled for subdomain 2.
+%          Subdomain 1 would have interface only along j=1. Therefore, only 1st row would be filled for subdomain 1) 
 
 %%% Variables in this function:
 % elem_len: number of elements in that specific subdomain
 % tri_int_area: stores the value of area of the triangles lying on the considered interface
 % partmat.artbound: lists all the interface triangles(and their corresponding nodes) for the given subdomain
 % partmat.art_tri_SD{j}: lists out the triangles(and their corresponding nodes)lying on the artificial boundary "ij"
-% partmat.art_elem_SD{j}: lists out the elements correspoding to the triangles mentioned in the previous line
+% partmat.art_elem_SD{j}: lists out the elements correspoding to the triangles lying on the artificial boundary "ij"
 % bound_tri(i): structure formed to store the 4 values required for evaluating area of a triangle
 %%% The four values are a,b,c and s. 
 
 
-% beta=1/(mesh_spacing^2);
-% alpha=1/(mesh_spacing);
-% 
 NC=length(NodeCoord(:,1));
  elem_len=length(partmat.ECM(:,1));
      for j=1:N
-%         if j==(i-1) || j==(i+1)
             tri_int_area=zeros(1,1,elem_len); 
             ismem=ismember(partmat.art_bound,partmat.interface_g{j});
             indx=(ismem(:,1)==1 & ismem(:,2)==1 & ismem(:,3)==1);
@@ -65,9 +73,6 @@ NC=length(NodeCoord(:,1));
             
             RHS_extra=(1/beta)*added_term;
             ABC.RHS_extra{j}=RHS_extra;
-%         else
-%             continue
-%         end
     end
     
 end
